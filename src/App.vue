04e7d24b8b77e91abe5dfd95a9b4bd7c1d43f8c6
Francesca-Barcella <pollon.fra@gmail>
2022-11-16 13:02:21 +0100

    vite-boolflix initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script >
+import axios from 'axios'
+import { store } from './store.js'
+
+export default{
+  name:'App',
+  data (){
+    return{
+      store
+    }
+  },
+
+  methods:{
+    callApi(){
+
+      axios.get(url)
+      .then(response => {
+        console.log(response.data);
+      })
+
+      .catch(err => {
+        console.log(err.message);
+      })
+    }
+  }
+
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from 'vue'
+import axios from 'axios'
+
+export const state = reactive({
+    api_url:'https://api.themoviedb.org/3/search/movie?api_key=a3a7406bba95703d01d22d9638cfdcfe&language=en-US&query=Matrix&page=1&include_adult=false',
+    
+
+})
